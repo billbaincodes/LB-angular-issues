521dab4d08ec601e69114cd40066028130ff8b4c
bill <williamandrewbain@gmail.com>
2019-03-11 21:57:18 -0600

    add issue component and fetcher

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    issueList: [],
+    loaded: false
+  }
+
+  issueFetcher = () => {
+
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>
+          Check out these Angular Issues
+        </h2>
+        {this.state.loaded ? 
+        this.state.issueList :
+        'waiting on them results'}
+
       </div>
     );
   }
---
+++ src/components/Issue.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Issue = () => {
+  return(
+    <div>
+      <h3>Title</h3>
+      <p>Body</p>
+      <h4>user login</h4>
+      <h4>assignee login</h4>
+    </div>
+  )
+}
+
+export default Issue
